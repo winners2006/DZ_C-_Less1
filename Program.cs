195f36eb722574eeeb5c28e7b44d2e490469e873
Winners2006-2 <winners2006-2@mail.ru>
2024-03-11 21:32:07 +0300

    Start

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConApp1
+{
+	
+}
